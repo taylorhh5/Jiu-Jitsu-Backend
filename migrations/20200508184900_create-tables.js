e76bfd81dcc1fb03addcf3f3f8b203260a6d0420
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-05-11 17:34:48 -0400

    updated migrations

--- migrations/20200508184900_create-tables.js
+++ migrations/20200508184900_create-tables.js
@@ -1,23 +1,76 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("takedowns", (tbl) => {
+    return knex.schema.createTable("users", (tbl) => {
+        tbl.increments();
+        tbl.string("email", 128).notNullable().unique()
+        tbl.string("password", 128).notNullable();
+        tbl.string("image_url", 500)
+
+      })
+  .createTable("takedown", (tbl) => {
       tbl.increments();
-      tbl.string("name", 128).notNullable().unique();
+      tbl.string("name", 128).notNullable().unique()
+      tbl.text("description").notNullable()
+      tbl.string("image_url", 500)
+
+      
+      tbl.integer("takedown_user_id")
+      .unsigned()
+      .references("id")
+      .inTable("users")
+      .onDelete("RESTRICT")
+      .onUpdate("CASCADE")
     })
     .createTable("guard", (tbl) => {
       tbl.increments();
-      tbl.string("name", 128).notNullable().unique();
+      tbl.string("name", 128).notNullable().unique()
+      tbl.text("description").notNullable()
+      tbl.string("image_url", 500)
+
+      tbl.integer("guard_user_id")
+      .unsigned()
+      .references("id")
+      .inTable("users")
+      .onDelete("RESTRICT")
+      .onUpdate("CASCADE")
     })
     .createTable("mount", (tbl) => {
       tbl.increments();
-      tbl.string("name", 128).notNullable().unique();
+      tbl.string("name", 128).notNullable().unique()
+      tbl.text("description").notNullable()
+      tbl.string("image_url", 500)
+
+      tbl.integer("mount_user_id")
+      .unsigned()
+      .references("id")
+      .inTable("users")
+      .onDelete("RESTRICT")
+      .onUpdate("CASCADE")
     })
     .createTable("sidemount", (tbl) => {
       tbl.increments();
-      tbl.string("name", 128).notNullable().unique();
+      tbl.string("name", 128).notNullable().unique()
+      tbl.text("description").notNullable()
+      tbl.string("image_url", 500)
+
+      tbl.integer("sidemount_user_id")
+      .unsigned()
+      .references("id")
+      .inTable("users")
+      .onDelete("RESTRICT")
+      .onUpdate("CASCADE")
     })
     .createTable("back", (tbl) => {
       tbl.increments();
-      tbl.string("name", 128).notNullable().unique();
+      tbl.string("name", 128).notNullable().unique()
+      tbl.text("description").notNullable()
+      tbl.string("image_url", 500)
+
+      tbl.integer("back_user_id")
+      .unsigned()
+      .references("id")
+      .inTable("users")
+      .onDelete("RESTRICT")
+      .onUpdate("CASCADE")
     });
 };
 
@@ -27,5 +80,6 @@ exports.down = function (knex) {
     .dropTableIfExists("sidemount")
     .dropTableIfExists("mount")
     .dropTableIfExists("guard")
-    .dropTableIfExists("takedowns");
+    .dropTableIfExists("takedown")
+    .dropTableIfExists("users");
 };
