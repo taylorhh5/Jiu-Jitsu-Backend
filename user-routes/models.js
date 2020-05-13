6d63b1ac2c6f74f05102f10228ececcdf0feec52
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-05-13 17:44:03 -0400

    All get routes for moves complete

--- user-routes/models.js
+++ user-routes/models.js
@@ -2,13 +2,64 @@ const db = require('../dbConfig.js')
 
 module.exports={
     getTakedown,
-    getTakedownById
+    getTakedownById,
+    getGuard,
+    getGuardById,
+    getMount,
+    getMountById,
+    getBack,
+    getBackById,
+    getSidemount,
+    getSidemountById
 }
 
 function getTakedown(){
     return db('takedown');
 }
 
+//get by name
+// function getTakedownById(name){
+//     return db('takedown').where({name}).first();
+// }
+
 function getTakedownById(id){
     return db('takedown').where({id}).first();
+}
+
+
+//get all guard
+function getGuard(){
+    return db('guard');
+}
+
+function getGuardById(id){
+    return db('guard').where({id}).first();
+}
+
+//get all mount
+function getMount(){
+    return db('mount');
+}
+
+function getMountById(id){
+    return db('mount').where({id}).first();
+}
+
+//get all back
+function getBack(){
+    return db('back');
+}
+
+function getBackById(id){
+    return db('back').where({id}).first();
+}
+
+
+//get all sidemount
+function getSidemount(){
+    return db('sidemount');
+}
+
+function getSidemountById(id){
+    return db('sidemount').where({id}).first();
 }
--- user-routes/routes.js
+++ user-routes/routes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const Move = require('./models.js');
 
 //get all takedowns
-router.get('/takedowns', (req, res)=>{
+router.get('/takedown', (req, res)=>{
 
     Move
     .getTakedown()
@@ -14,5 +14,166 @@ router.get('/takedowns', (req, res)=>{
     })
 })
 
+//get takedown by name
+// router.get('/takedowns/:id', (req, res)=>{
 
+//     const name = req.params.id
+//     console.log (req.params)
+//     Move
+//     .getTakedownById(name)
+    
+//     .then(move =>{
+//         if (move){
+//         res.status(200).json(move);
+//         }else{
+//             res.status(201).json({err:`No takedown move with the id of ${name} exists`}) 
+//         }
+//     }).catch(err =>{
+//         res.status(500).json({err:'failed to get takedown'})
+//     })
+// })
+
+//get takedown by id
+router.get('/takedown/:id', (req, res)=>{
+
+    const id = req.params.id
+    console.log (req.params)
+    Move
+    .getTakedownById(id)
+    
+    .then(move =>{
+        if (move){
+        res.status(200).json(move);
+        }else{
+            res.status(201).json({err:`No takedown move with the id of ${id} exists`}) 
+        }
+    }).catch(err =>{
+        res.status(500).json({err:'failed to get takedown'})
+    })
+})
+
+//get all guard
+router.get('/guard', (req, res)=>{
+
+    Move
+    .getGuard()
+    .then(move =>{
+        res.status(200).json(move);
+    }).catch(err =>{
+        res.status(500).json({err:'failed to get guard'})
+    })
+})
+
+//get guard by id
+router.get('/guard/:id', (req, res)=>{
+
+    const id = req.params.id
+    console.log (req.params)
+    Move
+    .getGuardById(id)
+    
+    .then(move =>{
+        if (move){
+        res.status(200).json(move);
+        }else{
+            res.status(201).json({err:`No guard move with the id of ${id} exists`}) 
+        }
+    }).catch(err =>{
+        res.status(500).json({err:'failed to get guard move'})
+    })
+})
+
+
+//get all mount
+router.get('/mount', (req, res)=>{
+
+    Move
+    .getMount()
+    .then(move =>{
+        res.status(200).json(move);
+    }).catch(err =>{
+        res.status(500).json({err:'failed to get mount move'})
+    })
+})
+
+//get mount by id
+router.get('/mount/:id', (req, res)=>{
+
+    const id = req.params.id
+    console.log (req.params)
+    Move
+    .getMountById(id)
+    
+    .then(move =>{
+        if (move){
+        res.status(200).json(move);
+        }else{
+            res.status(201).json({err:`No mount move with the id of ${id} exists`}) 
+        }
+    }).catch(err =>{
+        res.status(500).json({err:'failed to get mount move'})
+    })
+})
+
+//get all guard
+router.get('/back', (req, res)=>{
+
+    Move
+    .getBack()
+    .then(move =>{
+        res.status(200).json(move);
+    }).catch(err =>{
+        res.status(500).json({err:'failed to get back move'})
+    })
+})
+
+//get back by id
+router.get('/back/:id', (req, res)=>{
+
+    const id = req.params.id
+    console.log (req.params)
+    Move
+    .getBackById(id)
+    
+    .then(move =>{
+        if (move){
+        res.status(200).json(move);
+        }else{
+            res.status(201).json({err:`No back move with the id of ${id} exists`}) 
+        }
+    }).catch(err =>{
+        res.status(500).json({err:'failed to get back move'})
+    })
+})
+
+//get all guard
+router.get('/sidemount', (req, res)=>{
+
+    Move
+    .getSidemount()
+    .then(move =>{
+        res.status(200).json(move);
+    }).catch(err =>{
+        res.status(500).json({err:'failed to get sidemount move'})
+    })
+})
+
+//get guard by id
+router.get('/sidemount/:id', (req, res)=>{
+
+    const id = req.params.id
+    console.log (req.params)
+    Move
+    .getSidemountById(id)
+    
+    .then(move =>{
+        if (move){
+        res.status(200).json(move);
+        }else{
+            res.status(201).json({err:`No sidemount move with the id of ${id} exists`}) 
+        }
+    }).catch(err =>{
+        res.status(500).json({err:'failed to get sidemount move'})
+    })
+})
 module.exports=router;
