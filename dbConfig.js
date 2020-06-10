0e7ac3fd25dea509d0f666923684ee0ff8a1e682
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-06-10 12:25:40 -0400

    Corrected variable name in dbConfig

--- dbConfig.js
+++ dbConfig.js
@@ -2,6 +2,6 @@ const knex = require('knex');
 
 const knexConfig = require('./knexfile.js');
 
-const dbENnv = process.DB_ENV || 'development';
+const dbEnv = process.DB_ENV || 'development';
 
 module.exports = knex(knexConfig[dbEnv]);
