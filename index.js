83b6a3a548ebea2b3219e74193ceb4705c5c037f
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-06-10 11:02:41 -0400

    Changed seed files

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ server.get('/', (req, res)=>{
     res.send("working");
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () =>
 console.log("Server running on 5000")
--- seeds/04-mount.js
+++ seeds/04-mount.js
@@ -5,7 +5,7 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('mount').insert([
-        {name:'Arm bar', description:'Put your right hand on your opponents chest. Grab their right arm with your left. Put all your weight on your opponents chest and swing your left foot around the top of your oponents head. You should now be in a sitting position, perpendicular to your opponent, with your left leg across their neck and right leg across their chest. Holding your opponents right arms close to your chest and making sure their thumb is pointing up, lay back and lift your hips off the ground until they tap.', image_url:'pixabay.com/photo/2020/02/11/03/51/arts-4838225_960_720.jpg', user_id:1}
+        {name:'Arm bar', description:'Put your right hand on your opponents chest. Grab their right arm with your left. Put all your weight on your opponents chest and swing your left foot around the top of your oponents head. You should now be in a sitting position, perpendicular to your opponent, with your left leg across their neck and right leg across their chest. Holding your opponents right arms close to your chest and making sure their thumb is pointing up, lay back and lift your hips off the ground until they tap.', image_url:'https://cdn.pixabay.com/photo/2020/02/11/03/51/arts-4838225_960_720.jpg', user_id:1}
       ]);
     });
 };
