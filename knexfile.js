4e3213aff4c08480c0f03314be4e445531f776ed
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-06-10 13:12:51 -0400

    changed production settings in knexgile

--- knexfile.js
+++ knexfile.js
@@ -20,6 +20,10 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL, 
+    pool:{
+      min: 2,
+      max: 10
+    },
     migrations: {
       directory: './migrations',
     },
