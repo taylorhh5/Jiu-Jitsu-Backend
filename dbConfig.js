25b83e02d98987da1715bb4fca6c97fed876c0ee
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-06-10 13:16:05 -0400

    changed dbConfig to run production

--- dbConfig.js
+++ dbConfig.js
@@ -2,6 +2,6 @@ const knex = require('knex');
 
 const knexConfig = require('./knexfile.js');
 
-const dbEnv = process.DB_ENV || 'development';
+const dbEnv = process.DB_ENV || 'production';
 
 module.exports = knex(knexConfig[dbEnv]);
