a0e8f2e90e0ec22462153723a77e413c35530f33
Taylor <52665999+taylorhh5@users.noreply.github.com>
2021-03-10 13:34:55 -0500

    Edited env

--- knexfile.js
+++ knexfile.js
@@ -19,11 +19,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    // connection: process.env.DATABASE_URL, 
-    connection: { 
-      connectionString: pgConnection, 
-      ssl: { rejectUnauthorized: false }
-    },
+    connection: process.env.DATABASE_URL, 
     pool:{
       min: 2,
       max: 10
